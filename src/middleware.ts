d58046049d719222c7bed221e2532aae684cc329
Ammarbinyasar <miniprojectgrp14@gmail.com>
2025-02-18 23:54:26 +0530

    first commit

---
+++ src/middleware.ts
@@ -0,0 +1,51 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { routeAccessMap } from "./lib/settings";
+import { NextResponse } from "next/server";
+
+const matchers = Object.keys(routeAccessMap).map((route) => ({
+  matcher: createRouteMatcher([route]),
+  allowedRoles: routeAccessMap[route],
+}));
+
+console.log(matchers);
+
+export default clerkMiddleware((auth, req) => {
+  // if (isProtectedRoute(req)) auth().protect()
+
+  const { sessionClaims } = auth();
+
+  const role = (sessionClaims?.metadata as { role?: string })?.role;
+  console.log("User Role:", role);
+
+
+});
+
+  // for (const { matcher, allowedRoles } of matchers) {
+  //   if (matcher(req) && !allowedRoles.includes(role!)) {
+  //     return NextResponse.redirect(new URL(`/${role}`, req.url));
+  //   }
+  // }
+
+//   for (const { matcher, allowedRoles } of matchers) {
+//   if (matcher(req)) {
+//     if (!role) {
+//       console.warn("User role is undefined, redirecting to /login.");
+//       return NextResponse.redirect(new URL(`/Login`, req.url));
+//     }
+    
+//     if (!allowedRoles.includes(role)) {
+//       return NextResponse.redirect(new URL(`/${role}`, req.url));
+//     }
+//   }
+// }
+
+
+  // console.log(sessionClaims)
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
+};
